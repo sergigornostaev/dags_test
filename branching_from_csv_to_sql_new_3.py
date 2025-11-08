162ef36fb266661580291354cacdbd10fe8d2fc5
sergigornostaev <sergi.gornostaev@yandex.ru>
2025-11-08 21:22:12 +0300

    int_rebase_2

--- branching_from_csv_to_sql_new_3.py
+++ branching_from_csv_to_sql_new_3.py
@@ -13,6 +13,8 @@ print('cp1')
 
 print('int_rebase')
 
+print('int_rebase_2')
+
 def check_file_exists():
     """Проверяет наличие файла и возвращает следующий task_id"""
     file_path =  os.path.expanduser('~/datasets_from_net/for_datalens.csv')
