73220cddd337b52d53693d2f6cdb4f4ae30252b7
sergigornostaev <sergi.gornostaev@yandex.ru>
2025-11-08 17:04:32 +0300

    8

--- branching_from_csv_to_sql_new_3.py
+++ branching_from_csv_to_sql_new_3.py
@@ -23,6 +23,8 @@ print('6')
 
 print('7')
 
+print('8')
+
 def check_file_exists():
     """Проверяет наличие файла и возвращает следующий task_id"""
     file_path =  os.path.expanduser('~/datasets_from_net/for_datalens.csv')
